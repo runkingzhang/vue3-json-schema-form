7adbfde39c4ab501f766ee9fbb45c2c263290f17
zhangrunke <zrk@ihotel.cn>
2022-04-14 11:22:53 +0800

    脚手架提交

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+interface Config {
+  name: string
+}
+
+export default defineComponent({
+  name: 'App',
+  props: {
+    age: {
+      type: Number as PropType<number>,
+    },
+    config: {
+      type: Object as PropType<Config>,
+      required: true,
+    },
+  },
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: 'zhangrunke',
+    }
+  },
+  mounted() {
+    this.config
+  },
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp,defineComponent,h } from 'vue'
+import App from './App.vue'
+
+// const App = defineComponent({
+//     render(]h) {
+//         return h()
+//     },
+// })
+createApp(App).mount('#app')
